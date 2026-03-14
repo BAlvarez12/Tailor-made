0023956825ae44641064b45c7513eaf90a43dc6d
BAlvarez12 <160418757+BAlvarez12@users.noreply.github.com>
2026-03-14 12:06:57 -0600

    base de datos 1

---
+++ beautybell_1.sql
@@ -0,0 +1,413 @@
+CREATE DATABASE  IF NOT EXISTS `beautybell` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `beautybell`;
+-- MySQL dump 10.13  Distrib 8.0.36, for Win64 (x86_64)
+--
+-- Host: localhost    Database: beautybell
+-- ------------------------------------------------------
+-- Server version	8.0.37
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `categorias_material`
+--
+
+DROP TABLE IF EXISTS `categorias_material`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `categorias_material` (
+  `categoria_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_categoria` varchar(100) DEFAULT NULL,
+  `descripcion_categoria` varchar(300) DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`categoria_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `categorias_material`
+--
+
+LOCK TABLES `categorias_material` WRITE;
+/*!40000 ALTER TABLE `categorias_material` DISABLE KEYS */;
+/*!40000 ALTER TABLE `categorias_material` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente_prenda`
+--
+
+DROP TABLE IF EXISTS `cliente_prenda`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente_prenda` (
+  `cliente_prenda_id` int NOT NULL AUTO_INCREMENT,
+  `cliente_id` int NOT NULL,
+  `prenda_id` int NOT NULL,
+  `titulo` varchar(150) DEFAULT NULL,
+  `estado` int DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int NOT NULL,
+  PRIMARY KEY (`cliente_prenda_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente_prenda`
+--
+
+LOCK TABLES `cliente_prenda` WRITE;
+/*!40000 ALTER TABLE `cliente_prenda` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente_prenda` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cliente_prenda_medidas`
+--
+
+DROP TABLE IF EXISTS `cliente_prenda_medidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cliente_prenda_medidas` (
+  `cliente_medida_id` int NOT NULL AUTO_INCREMENT,
+  `cliente_prenda_id` int NOT NULL,
+  `tipo_medida_id` int NOT NULL,
+  `unidad_id` int NOT NULL,
+  `valor` decimal(10,2) DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creado` int NOT NULL,
+  PRIMARY KEY (`cliente_medida_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cliente_prenda_medidas`
+--
+
+LOCK TABLES `cliente_prenda_medidas` WRITE;
+/*!40000 ALTER TABLE `cliente_prenda_medidas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `cliente_prenda_medidas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `clientes`
+--
+
+DROP TABLE IF EXISTS `clientes`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `clientes` (
+  `cliente_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_cliente` varchar(50) DEFAULT NULL,
+  `apellido_cliente` varchar(50) DEFAULT NULL,
+  `telefono` varchar(30) DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`cliente_id`),
+  KEY `usuario_creador_idx` (`usuario_creador`),
+  CONSTRAINT `usuario_creador` FOREIGN KEY (`usuario_creador`) REFERENCES `usuarios` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `clientes`
+--
+
+LOCK TABLES `clientes` WRITE;
+/*!40000 ALTER TABLE `clientes` DISABLE KEYS */;
+/*!40000 ALTER TABLE `clientes` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materiales`
+--
+
+DROP TABLE IF EXISTS `materiales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materiales` (
+  `material_id` int NOT NULL AUTO_INCREMENT,
+  `categoria_id` int DEFAULT NULL,
+  `nombre_material` varchar(150) DEFAULT NULL,
+  `descripcion_material` text,
+  `precio_unitario` decimal(10,2) DEFAULT NULL,
+  `referencia_compra` varchar(200) DEFAULT NULL,
+  `estado` int DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`material_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materiales`
+--
+
+LOCK TABLES `materiales` WRITE;
+/*!40000 ALTER TABLE `materiales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materiales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `materiales_img`
+--
+
+DROP TABLE IF EXISTS `materiales_img`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `materiales_img` (
+  `material_id` int NOT NULL,
+  `url_img` varchar(800) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `materiales_img`
+--
+
+LOCK TABLES `materiales_img` WRITE;
+/*!40000 ALTER TABLE `materiales_img` DISABLE KEYS */;
+/*!40000 ALTER TABLE `materiales_img` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permisos`
+--
+
+DROP TABLE IF EXISTS `permisos`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permisos` (
+  `permiso_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_permiso` varchar(50) DEFAULT NULL,
+  PRIMARY KEY (`permiso_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permisos`
+--
+
+LOCK TABLES `permisos` WRITE;
+/*!40000 ALTER TABLE `permisos` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permisos` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `permisos_rol`
+--
+
+DROP TABLE IF EXISTS `permisos_rol`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `permisos_rol` (
+  `permiso_id` int NOT NULL,
+  `rol_id` int DEFAULT NULL,
+  PRIMARY KEY (`permiso_id`),
+  KEY `permisos_rol_idx` (`rol_id`),
+  CONSTRAINT `permisos_rol` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`rol_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `permisos_rol`
+--
+
+LOCK TABLES `permisos_rol` WRITE;
+/*!40000 ALTER TABLE `permisos_rol` DISABLE KEYS */;
+/*!40000 ALTER TABLE `permisos_rol` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prendas`
+--
+
+DROP TABLE IF EXISTS `prendas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prendas` (
+  `prenda_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_prenda` varchar(100) DEFAULT NULL,
+  `descripcion_prenda` varchar(255) DEFAULT NULL,
+  `estado` int DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`prenda_id`),
+  KEY `usuario_creador_idx` (`usuario_creador`),
+  CONSTRAINT `usuario_creador2` FOREIGN KEY (`usuario_creador`) REFERENCES `usuarios` (`usuario_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prendas`
+--
+
+LOCK TABLES `prendas` WRITE;
+/*!40000 ALTER TABLE `prendas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prendas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `prendas_img`
+--
+
+DROP TABLE IF EXISTS `prendas_img`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `prendas_img` (
+  `prendas_img_id` int NOT NULL AUTO_INCREMENT,
+  `prenda_id` int NOT NULL,
+  `url_img` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`prendas_img_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `prendas_img`
+--
+
+LOCK TABLES `prendas_img` WRITE;
+/*!40000 ALTER TABLE `prendas_img` DISABLE KEYS */;
+/*!40000 ALTER TABLE `prendas_img` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+  `rol_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_rol` varchar(50) DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  PRIMARY KEY (`rol_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'Administrador',1,NULL);
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `tipo_medidas`
+--
+
+DROP TABLE IF EXISTS `tipo_medidas`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `tipo_medidas` (
+  `tipo_medida_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_tipo_medida` varchar(100) DEFAULT NULL,
+  `descripcion_tipo_medida` varchar(300) DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`tipo_medida_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `tipo_medidas`
+--
+
+LOCK TABLES `tipo_medidas` WRITE;
+/*!40000 ALTER TABLE `tipo_medidas` DISABLE KEYS */;
+/*!40000 ALTER TABLE `tipo_medidas` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `unidades_medida`
+--
+
+DROP TABLE IF EXISTS `unidades_medida`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `unidades_medida` (
+  `unidad_id` int unsigned NOT NULL AUTO_INCREMENT,
+  `nombre_unidad` varchar(20) NOT NULL,
+  `simbolo_unidad` varchar(10) NOT NULL,
+  PRIMARY KEY (`unidad_id`),
+  UNIQUE KEY `nombre` (`nombre_unidad`),
+  UNIQUE KEY `simbolo` (`simbolo_unidad`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `unidades_medida`
+--
+
+LOCK TABLES `unidades_medida` WRITE;
+/*!40000 ALTER TABLE `unidades_medida` DISABLE KEYS */;
+/*!40000 ALTER TABLE `unidades_medida` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `usuarios`
+--
+
+DROP TABLE IF EXISTS `usuarios`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `usuarios` (
+  `usuario_id` int NOT NULL AUTO_INCREMENT,
+  `nombre_usuario` varchar(45) DEFAULT NULL,
+  `apellido_usuario` varchar(45) DEFAULT NULL,
+  `usuario` varchar(45) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(50) DEFAULT NULL,
+  `estado` int DEFAULT NULL,
+  `rol_id` int DEFAULT NULL,
+  `fecha_creado` datetime DEFAULT NULL,
+  `usuario_creador` int DEFAULT NULL,
+  PRIMARY KEY (`usuario_id`),
+  KEY `rol_idx` (`rol_id`),
+  CONSTRAINT `rol` FOREIGN KEY (`rol_id`) REFERENCES `roles` (`rol_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `usuarios`
+--
+
+LOCK TABLES `usuarios` WRITE;
+/*!40000 ALTER TABLE `usuarios` DISABLE KEYS */;
+INSERT INTO `usuarios` VALUES (1,'Bryann','Alvarez','balvarez','$2b$10$FRmykqJ96zXoe4GJALZV5.Bcg2ioOyywJWGF5Cu9a3bO6Sk/HLh7y','balvarez@gmail.com',1,1,NULL,NULL),(3,'Nelly','Lopez','nlopez','$2b$10$C3C1DzU5JcU/eAWHnUVGIOFxfJPqW7PZD8wggT6fHJe5qb6XhotvK','nlopez@gmail.com',1,1,'2026-03-14 11:46:59',1),(4,'Luis','Ajim','lajim','$2b$10$.leW11H9fZBZ4d/sHgWTveZqhjWNbKSMSoxSHBammHpEepQHA1Pvi','lajim@gmail.com',1,1,'2026-03-14 11:50:34',1),(5,'Christian','Garcia','cgarcia','$2b$10$gSeG1dWYS0D634pC1xNPaucq3M57PlARAOJ3JECA9ZhKdScWNebV6','christian@gmail.com',1,1,'2026-03-14 11:52:46',1);
+/*!40000 ALTER TABLE `usuarios` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'beautybell'
+--
+
+--
+-- Dumping routines for database 'beautybell'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-03-14 12:06:14
