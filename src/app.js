d5d7a8c67378854994bccb910264168585034ed3
Christian3033 <christian300r@gmail.com>
2026-03-20 21:08:35 -0600

    Clientes, unidades de medida y tipos de medida

--- src/app.js
+++ src/app.js
@@ -1,26 +1,38 @@
+/* CONFIGURACIÓN INICIAL */
 require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 
-
 const app = express()
 
+/* CONFIGURACIÓN CORS */
 app.use(cors({
   origin: 'http://localhost:5173',
-  methods: ['GET', 'POST', 'PUT'],
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
 }))
 
+/* MIDDLEWARE */
 app.use(express.json())
 
+/* RUTA DE PRUEBA */
 app.get('/', (req, res) => {
   res.send('Backend funcionando')
 })
 
-
+/* RUTAS */
 app.use('/api/auth', require('./routes/auth'))
 app.use('/api/usuarios', require('./routes/usuarios'))
 app.use('/api/roles', require('./routes/roles'))
 
+/* RUTA CLIENTES */
+app.use('/api/clientes', require('./routes/clientes/clientes'))
 
+/* RUTA UNIDADES */
+app.use('/api/unidades', require('./routes/unidades/unidades'))
+
+/* RUTA TIPOS */
+app.use('/api/tipo-medidas', require('./routes/tipo_medidas/tipo_medidas'))
+
+/* EXPORTAR APP */
 module.exports = app
