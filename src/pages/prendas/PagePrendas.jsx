3eb0d2163cc677f0f7a8cf0da485978697a2ef99
Christian3033 <christian300r@gmail.com>
2026-03-21 18:10:28 -0600

    correccion

--- src/pages/prendas/PagePrendas.jsx
+++ src/pages/prendas/PagePrendas.jsx
@@ -351,7 +351,7 @@ function PrendasPage() {
                       className="prenda-card__button prenda-card__button--secondary"
                       onClick={() => handleVerImagenes(prenda)}
                     >
-                      Ver imágeneschris
+                      Ver imágenes
                     </button>
                   </div>
                 </div>
