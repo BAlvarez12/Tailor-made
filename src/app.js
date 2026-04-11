f07c94f143d496c7c89937adfea8e6f5062e649b
Christian3033 <christian300r@gmail.com>
2026-04-11 17:52:14 -0600

    [Update] se agrega Backend con cambios mas recientes (se realizaron unicamente cambios en la conexion a la BD y en el cors

--- src/app.js
+++ src/app.js
@@ -9,6 +9,7 @@ const app = express();
 // 🌐 CONFIGURACIÓN CORS
 // MIDDLEWARES
 app.use(cors({
+  //origin: 'http://localhost:5173',
   origin: 'http://localhost:5173',
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true
